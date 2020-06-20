2ae7417259577b7f70408b2d9f60c542f8e9e6ce
Carles San Agustin <carles.sanagustin@scrm.eu>
2020-06-20 10:27:35 +0200

    makefile update

--- Makefile
+++ Makefile
@@ -3,11 +3,15 @@
 -include ./mk/secure-ingress.mk
 -include ./mk/terraform.mk
 
-
 export project_prefix=test-carlesaks
 export project_sufix=ne-001
 
-export TF_VAR_office_pip=${OFFICE_PIP}
+export TF_VAR_project_prefix=${project_prefix}
+export TF_VAR_project_sufix=${project_sufix}
+
+AKS_RG=${project_prefix}-rg
+AKS_NAME=${project_prefix}-k8s
+
 
 OFFICE_PIP := $(shell curl -s https://checkip.amazonaws.com)
 whatismyip:
---
+++ mk/secure-ingress.mk
@@ -0,0 +1,62 @@
+# Get Azure Application Gateway public ip
+show_appgw_ip:
+	@echo ">> appgw_ip: "
+	@az network public-ip show \
+	--name ${project_prefix}-appgw-pip \
+	--resource-group ${AKS_RG} \
+	--subscription ${ARM_SUBSCRIPTION_ID} \
+	| jq '.ipAddress' | tr -d '"'
+
+show_pips: show_nginx_ingress_ip show_appgw_ip
+
+
+# output: aks_node_resource_group = 
+TF_VAR_aks_node_resource_group = $(shell az aks show --name ${project_prefix}-k8s --resource-group ${AKS_RG} --subscription ${ARM_SUBSCRIPTION_ID} | jq '.nodeResourceGroup')
+TF_VAR_aks_nsg_name = $(shell az network nsg list --resource-group ${TF_VAR_aks_node_resource_group} --subscription ${ARM_SUBSCRIPTION_ID} | jq '.[].name')
+# --destination-address-prefixes = 'VirtualNetwork', 'AzureLoadBalancer', 'Internet' or '*'
+add_rule_http:
+	az network nsg rule create \
+	--subscription ${ARM_SUBSCRIPTION_ID} \
+	--resource-group ${TF_VAR_aks_node_resource_group} \
+	--nsg-name ${TF_VAR_aks_nsg_name} \
+	--name office-pip-http \
+	--priority 100 \
+	--access Allow \
+	--destination-address-prefixes AzureLoadBalancer \
+	--destination-port-ranges 80 \
+	--direction Inbound \
+	--protocol TCP \
+	--source-port-ranges '*' \
+	--source-address-prefixes ${OFFICE_PIP}
+
+add_rule_https:
+	az network nsg rule create \
+	--subscription ${ARM_SUBSCRIPTION_ID} \
+	--resource-group ${TF_VAR_aks_node_resource_group} \
+	--nsg-name ${TF_VAR_aks_nsg_name} \
+	--name office-pip-https \
+	--priority 101 \
+	--access Allow \
+	--destination-address-prefixes AzureLoadBalancer \
+	--destination-port-ranges 443 \
+	--direction Inbound \
+	--protocol TCP \
+	--source-port-ranges '*' \
+	--source-address-prefixes ${OFFICE_PIP}
+
+add_rule_deny_any:
+	az network nsg rule create \
+	--subscription ${ARM_SUBSCRIPTION_ID} \
+	--resource-group ${TF_VAR_aks_node_resource_group} \
+	--nsg-name ${TF_VAR_aks_nsg_name} \
+	--name office-pip-deny-any \
+	--priority 110 \
+	--access Deny \
+	--destination-address-prefixes '*' \
+	--destination-port-ranges '*' \
+	--direction Inbound \
+	--protocol '*' \
+	--source-port-ranges '*' \
+	--source-address-prefixes Internet
+
+add_rules: add_rule_https add_rule_http add_rule_deny_any
